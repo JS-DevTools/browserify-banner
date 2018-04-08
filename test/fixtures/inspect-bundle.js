3a4d9e060736e0cbb4c1759267a88c045b342374
James Messinger <james.messinger@gmail.com>
2018-04-08 12:33:37 -0500

    Fixed tests that were failing due to changes in the Browserify UMD prelude

--- test/fixtures/inspect-bundle.js
+++ test/fixtures/inspect-bundle.js
@@ -15,12 +15,6 @@ module.exports = inspectBundle;
  */
 function inspectBundle (bundle, options) {
   let actualParts = splitBundleIntoParts(bundle);
-
-  // If the last part is just the terminator, then ignore it
-  if (actualParts[actualParts.length - 1] === ';\n') {
-    actualParts.pop();
-  }
-
   actualParts.should.deep.equal(options.parts);
 }
 
@@ -92,14 +86,13 @@ const ALL_PARTS = [
   {
     name: 'prelude',
     contents:
-`(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require==\"function\"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error(\"Cannot find module '\"+o+\"'\");throw f.code=\"MODULE_NOT_FOUND\",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require==\"function\"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
+`(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 `
   },
 
   {
     name: 'postlude',
-    contents:
-`},{}]},{},[2])`
+    pattern: /\},\{\}\]\},\{\},\[2\]\);?\n?/
   },
 
   {
@@ -110,16 +103,13 @@ const ALL_PARTS = [
 
   {
     name: 'umd postlude',
-    contents:
-`(2)
-});`
+    pattern: /\(2\)\n\}\);\n?/
   },
 
   {
     name: 'sourcemap',
     contents:
-`
-//# sourceMappingURL=bundle.js.map
+`//# sourceMappingURL=bundle.js.map
 `
   },
 
